59eb9163cbe70189515fc9131ab99456a5279283
Kaleb Klein <apollyon156@protonmail.com>
2023-04-30 11:23:13 -0400

    Add mirrorlist command

---
+++ functions/mirrors.fish
@@ -0,0 +1,23 @@
+function mirrors -d 'Update pacman mirrorlist'
+  set -l MPATH /etc/pacman.d/mirrorlist
+
+  read -l MOVE -P 'Backup current mirrorlist? [Y/n] '
+  switch $MOVE
+    case Y y
+      sudo cp $MPATH $MPATH.bak
+  end
+
+  reflector -c US -f 10 -l 20 --sort score --save $MPATH --verbose
+
+  if test $status -eq 0
+    read -l UP -P 'Completed. Would you like to update now? [y/N] '
+    switch $UP
+      case Y y
+        update
+    end
+  else
+    echo 'An error occurred whilst doing this. Restoring backup'
+    sudo rm $MPATH
+    sudo cp $MPATH.bak $MPATH
+  end
+end
