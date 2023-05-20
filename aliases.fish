dad6766f4decdfba9910a07e2e575c34edd9b203
Kaleb Klein <apollyon156@proton.me>
2023-05-20 11:21:41 -0400

    Move aliases to their own file

---
+++ aliases.fish
@@ -0,0 +1,27 @@
+# random aliases
+abbr -a ycu yay-clean -u
+abbr -a neo neofetch
+abbr -a yc yay-clean
+abbr -a :q exit
+abbr -a x startx
+abbr -a w startw
+abbr -a bt bpytop
+abbr -a cls clear
+abbr -a vs v -s $argv
+abbr -a kfish kate $HOME/.config/fish
+abbr -a st speedtest --bytes --secure
+abbr -a reload source $HOME/.config/fish/config.fish
+
+# spotifyd
+abbr -a enspot spot enable
+abbr -a disspot spot disable
+
+# baloo
+abbr -a enbaloo balooctl enable
+abbr -a disbaloo balooctl disable
+
+# vksetup
+abbr -a vks vksetup -p $argv
+abbr -a vkr vkremove -p $argv
+abbr -a vkms vkmultisetup -p $argv
+abbr -a vkmr vkmultiremove -p $argv
--- config.fish
+++ config.fish
@@ -3,33 +3,10 @@ if status is-interactive
 end
 
 set -U fish_greeting ""
-
 set -x EDITOR nvim
-abbr -a ycu yay-clean -u
-abbr -a neo neofetch
-abbr -a yc yay-clean
-abbr -a :q exit
-abbr -a x startx
-abbr -a w startw
-abbr -a bt bpytop
-abbr -a cls clear
-abbr -a vs v -s $argv
-abbr -a kfish kate $HOME/.config/fish
-abbr -a st speedtest --bytes --secure
 
-# spotifyd
-abbr -a enspot spot enable
-abbr -a disspot spot disable
-
-# baloo
-abbr -a enbaloo balooctl enable
-abbr -a disbaloo balooctl disable
-
-# vksetup
-abbr -a vks vksetup -p $argv
-abbr -a vkr vkremove -p $argv
-abbr -a vkms vkmultisetup -p $argv
-abbr -a vkmr vkmultiremove -p $argv
+# source aliases file
+source $HOME/.config/fish/aliases.fish
 
 # Get a plasma session going on TTY login
 if test ! -n "$DISPLAY"
