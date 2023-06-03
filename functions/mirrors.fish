a1ba9b5846703e5016a3431a83d66f37b8ada2fc
Kaleb Klein <apollyon156@proton.me>
2023-06-03 06:45:59 -0400

    Update mirrors to allow restoring of backup copy

--- functions/mirrors.fish
+++ functions/mirrors.fish
@@ -1,23 +1,41 @@
 function mirrors -d 'Update pacman mirrorlist'
+  argparse -n mirrors 'r/restore' -- $argv
+    or return
+
   set -l MPATH /etc/pacman.d/mirrorlist
 
-  read -l MOVE -P 'Backup current mirrorlist? [Y/n] '
-  switch $MOVE
-    case Y y
-      sudo cp $MPATH $MPATH.bak
-  end
-
-  sudo reflector -c US -f 10 -l 20 --sort score --save $MPATH --verbose
-
-  if test $status -eq 0
-    read -l UP -P 'Completed. Would you like to update now? [y/N] '
-    switch $UP
-      case Y y
-        update
+  if set -q _flag_r
+    if test -f $MPATH.bak
+      sudo rm -rf $MPATH
+      sudo mv $MPATH.bak $MPATH
+      read -l REUP -P 'Mirrorlist backup restored. Run a system update now? [Y/n] '
+      switch $REUP
+        case Y y
+          update
+      end
+    else
+      echo 'There is no mirrorlist backup to restore from!'
     end
   else
-    echo 'An error occurred whilst doing this. Restoring backup'
-    sudo rm $MPATH
-    sudo cp $MPATH.bak $MPATH
+
+    read -l MOVE -P 'Backup current mirrorlist? [Y/n] '
+    switch $MOVE
+      case Y y
+        sudo cp $MPATH $MPATH.bak
+    end
+
+    sudo reflector -c US -f 10 -l 20 --sort score --save $MPATH --verbose
+
+    if test $status -eq 0
+      read -l UP -P 'Completed. Would you like to update now? [y/N] '
+      switch $UP
+        case Y y
+          update
+      end
+    else
+      echo 'An error occurred whilst doing this. Restoring backup'
+      sudo rm $MPATH
+      sudo cp $MPATH.bak $MPATH
+    end
   end
 end
