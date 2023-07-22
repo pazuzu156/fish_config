db06e49b13393ba3fd7523d98cb0ae547c665ffa
Kaleb Klein <apollyon156@proton.me>
2023-07-22 19:47:29 -0400

    Change update script to use paru

--- config.fish
+++ config.fish
@@ -13,24 +13,24 @@ set -l GE_VERSION 8-8
 fish_add_path --path $HOME/.config/heroic/tools/wine/Wine-GE-Proton$GE_VERSION/bin
 set -e GE_VERSION
 
-# Get a plasma session going on TTY login
+# Get a plasma session going on TTY login (not using ATM)
 if test ! -n "$DISPLAY"
-  read -l SPS -P 'Need to start plasma? [Y/n] '
-  switch $SPS
+	#read -l SPS -P 'Need to start plasma? [Y/n] '
+	#switch $SPS
     # Answered with a yes and no given session
-    case Y y
+    #case Y y
       # ask which session to start with
-      read -l SPS_TYPE -P 'Which one; X11[x] or Wayland[w]? '
-      switch $SPS_TYPE
-        case X x
-          startx
-        case W w
-          startplasma-wayland
-      end
+      # read -l SPS_TYPE -P 'Which one; X11[x] or Wayland[w]? '
+      #switch $SPS_TYPE
+      #case X x
+      #          startx
+	  #case W w
+	  #   startplasma-wayland
+	  #end
     # Answered with a yes and a session, just go ahead and launch the requested one
-    case YW yw Yw yW
-      startplasma-wayland
-    case YX yx Yx yX
-      startx
-  end
+    #case YW yw Yw yW
+    #      startplasma-wayland
+      #case YX yx Yx yX
+      #      startx
+      #end
 end
--- functions/update.fish
+++ functions/update.fish
@@ -1,6 +1,22 @@
 function update --description 'Perform a quick update'
+  read -l MIRRORS -P 'Do you want to update the mirrorlist first? [Y/n] '
+  switch $MIRRORS
+    case Y y
+      mirrors -n
+  end
+
+  sudo pacman -Syu --noconfirm
+
+  read -l UPDAUR -P 'Update AUR packages? [y/N] '
+  switch $UPDAUR
+    case Y y
+      paru -Sua
+  end
+
+  # read -P 'Press [ENTER] to quit'
+
   # update system
-  yay --answerdiff None --answerupgrade Repo --answeredit None --answerclean All --batchinstall -Syu
+  # yay --answerdiff None --answerupgrade Repo --answeredit None --answerclean All --batchinstall -Syu
   # upgrade flatpaks
-  flatpak update --noninteractive
+  # flatpak update --noninteractive
 end
